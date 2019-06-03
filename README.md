7e6daa747271ed4f83cb401347f5ebd5f8dbd99c
seanemac <S.mccann11@nuigalway.ie>
2019-06-03 22:19:16 +0100

    adding a Readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Irish rent data visualisation
+
+### Visualise CSV data from the Central Statistics Office using pandas and matplotlib
+![example](result.png)
--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-cities = [
-    "Dublin",
-    "Galway City",
-    "Cork City",
-    "Limerick City"
-]
+cities = [ "Dublin", "Galway City",
+    "Cork City", "Limerick City"]
 
 df = pd.read_csv('RentByQuarter.csv')  # import csv
 df = df[df.num != '".."']  # drop null rows 750,000 -> 220,000
@@ -17,11 +13,13 @@ df['num'] = df['num'].apply(pd.to_numeric)  # change num to float
 
 cityData = []
 for i in range(len(cities)):
-    cityData.append(df.loc[(df['Property Type'] == 'All property types') & (df['Number of Bedrooms'] == 'All bedrooms') & (df['Location'] == cities[i])])
+    cityData.append(df.loc[(df['Property Type'] == 'All property types') 
+    & (df['Number of Bedrooms'] == 'All bedrooms') & (df['Location'] == cities[i])])
     plt.plot(cityData[i]['Quarter'], cityData[i]['num'])
 
 
-plt.legend(['Dublin', 'Galway', 'Cork', 'Limerick', 'Athlone', 'Waterford'], loc='upper left', fontsize='xx-large')
+plt.legend(['Dublin', 'Galway', 'Cork', 'Limerick', 'Athlone', 'Waterford'],
+     loc='upper left', fontsize='xx-large')
 plt.ylabel('Rent €')
 plt.xlabel('Year')
 
